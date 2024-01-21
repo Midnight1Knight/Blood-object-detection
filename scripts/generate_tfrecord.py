e0879c39adbb7661dc9b92c65fd0ce81677ea225
Midnight1Knight <andrkar008@gmail.com>
2024-01-21 22:17:11 +0300

    Make foundation for developing

--- scripts/generate_tfrecord.py
+++ scripts/generate_tfrecord.py
@@ -107,7 +107,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
